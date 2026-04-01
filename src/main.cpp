61797112f591d1f816c8be39f3613ff4e3da8d84
VATSU_04 <vatsumalambo@gmail.com>
2026-04-01 08:46:31 +0200

    Réorganisation de la fonction setup pour améliorer la lisibilité et ajout d'un retour à la ligne à la fin du fichier

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,13 @@ const char* password = "";
 void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid, password);
-}
-
-void loop() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connexion...");
   }
 
   Serial.println("Connecté");
+}
+
+void loop() {
 }
