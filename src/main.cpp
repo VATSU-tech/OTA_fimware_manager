51e5c466a293fe91d9d6b5a62d3ef8f60642e01b
VATSU_04 <vatsumalambo@gmail.com>
2026-04-08 03:43:49 +0200

    led fadden

--- src/main.cpp
+++ src/main.cpp
@@ -1,113 +1,27 @@
-// #include <Arduino.h>
-// #include <ESP8266WiFi.h>
-// #include <ESP8266HTTPClient.h>
-// #include <ESP8266httpUpdate.h>
-// #include <WiFiClientSecureBearSSL.h>
-
-// const char* ssid = "The Real Bienv";
-// const char* password = "8d9610E<>"; // TODO: mets le vrai mot de passe WiFi
-
-// // NOTE:
-// // Les fichiers générés dans .pio/build ne sont pas dans le repo GitHub.
-// // Mets ton firmware et version dans un dossier commité, par ex:
-// // firmware/version.txt et firmware/firmware.bin
-// const char* version_url =
-//     "https://raw.githubusercontent.com/VATSU-tech/OTA_fimware_manager/main/firmware/version.txt";
-// const char* firmware_url =
-//     "https://raw.githubusercontent.com/VATSU-tech/OTA_fimware_manager/main/firmware/firmware.bin";
-
-// const char* currentVersion = "1.0.0";
-
-// void checkUpdate();
-// void updateFirmware();
-
-// void setup() {
-//   Serial.begin(9600);
-//   WiFi.mode(WIFI_STA);
-//   WiFi.begin(ssid, password);
-
-//   while (WiFi.status() != WL_CONNECTED) {
-//     delay(1000);
-//     Serial.println("Connexion...");
-//   }
-
-//   Serial.println("Connecté");
-
-//   checkUpdate();
-// }
-
-// void loop() {
-//   // rien ici
-// }
-
-// void checkUpdate() {
-//   std::unique_ptr<BearSSL::WiFiClientSecure> client(
-//       new BearSSL::WiFiClientSecure);
-//   client->setInsecure(); // TODO: remplace par un certificat si tu veux du TLS sûr
-
-//   HTTPClient http;
-//   if (!http.begin(*client, version_url)) {
-//     Serial.println("Erreur HTTP begin pour version_url");
-//     return;
-//   }
-
-//   int httpCode = http.GET();
-//   if (httpCode == 200) {
-//     String newVersion = http.getString();
-//     newVersion.trim();
-
-//     Serial.println("Version distante: " + newVersion);
-
-//     if (newVersion != currentVersion) {
-//       Serial.println("Mise à jour disponible !");
-//       updateFirmware();
-//     } else {
-//       Serial.println("Déjà à jour");
-//     }
-//   } else {
-//     Serial.printf("Erreur HTTP version: %d\n", httpCode);
-//   }
-
-//   http.end();
-// }
-
-// void updateFirmware() {
-//   std::unique_ptr<BearSSL::WiFiClientSecure> client(
-//       new BearSSL::WiFiClientSecure);
-//   client->setInsecure(); // TODO: remplace par un certificat si tu veux du TLS sûr
-
-//   // Optionnel: LED intégrée pendant l'update
-//   ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW);
-
-//   t_httpUpdate_return ret =
-//       ESPhttpUpdate.update(*client, firmware_url, currentVersion);
-
-//   switch (ret) {
-//     case HTTP_UPDATE_FAILED:
-//       Serial.printf("Echec: %s\n", ESPhttpUpdate.getLastErrorString().c_str());
-//       break;
-
-//     case HTTP_UPDATE_NO_UPDATES:
-//       Serial.println("Pas de maj");
-//       break;
-
-//     case HTTP_UPDATE_OK:
-//       Serial.println("Succès → reboot");
-//       break;
-//   }
-// }
-
 #include <Arduino.h>
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.println("Hello, world!");
+#define led D4
+#define interval 50
+
+bool state = false;
+unsigned int compteur = 200;
+
+unsigned long presentTime = 0;
+unsigned long previousTime = 0;
+
+void setup(){
+    Serial.begin(9600);
+    pinMode(led, OUTPUT);
 }
 
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+void loop(){
+    presentTime = millis();
+
+    if ((presentTime - previousTime) >= interval) {
+        state = compteur >= 255 || compteur <= 200 ? !state : state;
+        compteur = state ? compteur + 1 : compteur - 1;
+        analogWrite(led, compteur);
+        Serial.println(compteur);
+        previousTime = presentTime;
+    }
 }
